65974fe46e39d018a379bcbbde77b1d8dba311ea
pubrrr <pubr@posteo.de>
2026-01-05 14:03:21 +0100

    feat: fetch tournaments

--- updateJob/main.ts
+++ updateJob/main.ts
@@ -1,1 +1,6 @@
-// const API_TOKEN = "...";
+const API_TOKEN = process.env.API_TOKEN;
+const API_URL = 'https://api.tournament.io/v1/public';
+
+const response = await fetch(`${API_URL}/tournaments`, { headers: { Authorization: API_TOKEN } });
+
+console.log(await response.json());
