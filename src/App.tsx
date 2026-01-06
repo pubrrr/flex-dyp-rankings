fd5f530bf04a7768fb00a11cf5c0e94c37a95b65
pubrrr <pubr@posteo.de>
2026-01-06 16:25:29 +0100

    feat: PointsPerRankTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { type FC, Suspense, use } from 'react';
 import { type Result, resultSchema } from '../updateJob/resultType.ts';
-import { getPointsForRank } from './getPointsForRank.ts';
+import { DEFAULT_POINTS, getPointsForRank, POINTS_MAP } from './getPointsForRank.ts';
 
 export const App: FC = () => {
     const promise = fetch(`${import.meta.env.BASE_URL}2026.json`)
@@ -37,7 +37,18 @@ const YearDisplay: FC<YearDisplayProps> = ({ dataPromise }) => {
         }
     }
 
-    return <Leaderboard data={[...pointsByPlayerId.values()]} />;
+    return (
+        <div className='m-4'>
+            <details className='shadow-neutral border-base-300 collapse-arrow collapse mb-4 max-w-sm border'>
+                <summary className='collapse-title font-semibold'>Punkteschlüssel</summary>
+                <div className='collapse-content'>
+                    Die Tabelle zeigt wie viele Punkte man für welche Platzierung bekommt.
+                    <PointsPerRankTable />
+                </div>
+            </details>
+            <Leaderboard data={[...pointsByPlayerId.values()]} />
+        </div>
+    );
 };
 
 type LeaderboardProps = {
@@ -46,25 +57,55 @@ type LeaderboardProps = {
 
 const Leaderboard: FC<LeaderboardProps> = ({ data }) => {
     return (
-        <div className='rounded-box shadow-neutral border-base-300 m-4 max-w-2xl overflow-x-auto border shadow'>
+        <>
+            <h2 className='my-4 text-xl font-semibold'>Rangliste</h2>
+            <div className='rounded-box shadow-neutral border-base-300 max-w-xl overflow-x-auto border shadow'>
+                <table className='table'>
+                    <thead>
+                        <tr className='bg-primary/50'>
+                            <th>Rang</th>
+                            <th>Spieler</th>
+                            <th>Punkte</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data
+                            .sort((a, b) => b.points - a.points)
+                            .map((item, index) => (
+                                <tr key={item.playerName} className='hover:bg-base-200'>
+                                    <td className='text-base-content/40'>#{index}</td>
+                                    <td>{item.playerName}</td>
+                                    <td>{item.points}</td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
+};
+
+const PointsPerRankTable: FC = () => {
+    return (
+        <div className='rounded-box shadow-neutral border-base-300 m-4 max-w-sm overflow-x-auto border shadow'>
             <table className='table'>
                 <thead>
                     <tr className='bg-primary/50'>
-                        <th>Rank</th>
-                        <th>Player</th>
-                        <th>Points</th>
+                        <th>Platzierung</th>
+                        <th>Punkte</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {data
-                        .sort((a, b) => b.points - a.points)
-                        .map((item, index) => (
-                            <tr key={item.playerName} className='hover:bg-base-200'>
-                                <td>#{index}</td>
-                                <td>{item.playerName}</td>
-                                <td>{item.points}</td>
-                            </tr>
-                        ))}
+                    {[...POINTS_MAP.entries()].map(([rank, points]) => (
+                        <tr key={rank} className='hover:bg-base-200'>
+                            <td>{rank}</td>
+                            <td>{points}</td>
+                        </tr>
+                    ))}
+                    <tr className='hover:bg-base-200'>
+                        <td>&ge;{Math.max(...POINTS_MAP.keys()) + 1}</td>
+                        <td>{DEFAULT_POINTS}</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
